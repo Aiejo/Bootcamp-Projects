25a8afc4e87623fa3f55851807b6c230d52a6c5f
alejandro <alejandro9.ch321@gmail.com>
2022-08-24 11:29:03 -0500

    Segunda parte del proyecto: Ejericicios 1.6 a 1.11

--- src/index.js
+++ src/index.js
@@ -1,65 +1,87 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client';
+import React,{useState} from 'react'
 
-const Header = ({course}) =>{
+const Title = ({text}) =>{
   return (
-    <div>
-      <h1>{course.name}</h1>
-    </div>
+    <h1>{text}</h1>
   )
 }
 
-const Content = ({course}) =>{
+const Statistics = (props) => {
   return (
-    <div>
-      <Part part={course.parts[0].name} exercises={course.parts[0].exercises}/>
-      <Part part={course.parts[1].name} exercises={course.parts[1].exercises}/>
-      <Part part={course.parts[2].name} exercises={course.parts[2].exercises}/>
-    </div>
+    <tr>
+    <td>{props.text}</td>
+    <td style={{paddingLeft:'10px'}}>{props.value}</td>
+    </tr>
   )
 }
-
-const Part = ({part,exercises}) => {
+const Button = (props) => {
   return (
-    <div>
-      <p>{part} {exercises}</p>
-    </div>
-  )
-}
-const Total = ({course}) =>{
-  return (
-    <div>
-      <p>Number of exercises {course.parts.map(part=>part.exercises).reduce((acum,curr)=>acum+curr)}</p>
-    </div>
+    <button onClick={props.handleClick}>{props.type}</button>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [ 
-      {
-    name: 'Fundamentals of React',
-    exercises: 10,
-      },
-      {
-    name: 'Using props to pass data',
-    exercises: 7,
-      },
-      {
-    name: 'State of a component',
-    exercises: 14,
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all,setAll] = useState(0)
+
+  const avg = (good+-bad)/all ? (good-bad)/all : 0
+  const pos = `${good/all ? good/all*100 : 0} %`
+  
+  const handleButtonClick = (value) => {
+      switch (value){
+        case 1:
+          setGood(good+1)
+          setAll(all+1)
+          break;
+
+        case 0:
+          setNeutral(neutral+1)
+          setAll(all+1)
+          break;
+
+        case -1:
+          setBad(bad+1)
+          setAll(all+1)
+          break;
+
+        default:
+            break;
       }
-    ]
-  }
+    }
+
 
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course} />
-      <Total course={course}/>
+
+      <Title text="Give Feedback"/>
+      <Button type = {"good"} handleClick = {()=>handleButtonClick(1)}/>
+      <Button type = {"neutral"} handleClick = {()=>handleButtonClick(0)}/>
+      <Button type = {"bad"} handleClick = {()=>handleButtonClick(-1)}/>
+      <Title text="Statistics"/>
+      <table>
+        <tbody>
+          <Statistics text="Good" value={good}/>
+          <Statistics text="Neutral" value={neutral}/>
+          <Statistics text="Bad" value={bad}/>
+          <Statistics text="all" value={all}/>
+          <Statistics text="average" value={avg}/>
+          <Statistics text="positive" value={pos}/>
+        </tbody>
+      </table>
+
     </div>
+    
+
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
