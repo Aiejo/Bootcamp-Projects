0f60ddb5b83038cb1b4aeebd65b0283ce7a60171
alejandro <alejandro9.ch321@gmail.com>
2022-08-24 16:00:52 -0500

    Tercera parte del proyecto: Ejercicios 1.12 a 1.14

--- src/index.js
+++ src/index.js
@@ -1,87 +1,88 @@
 import ReactDOM from 'react-dom/client';
 import React,{useState} from 'react'
 
-const Title = ({text}) =>{
+const Title = ({text}) => {
   return (
     <h1>{text}</h1>
   )
 }
-
-const Statistics = (props) => {
-  return (
-    <tr>
-    <td>{props.text}</td>
-    <td style={{paddingLeft:'10px'}}>{props.value}</td>
-    </tr>
-  )
-}
 const Button = (props) => {
   return (
-    <button onClick={props.handleClick}>{props.type}</button>
+    <button onClick={props.handleClick}>{props.text}</button>
   )
 }
 
-const App = () => {
-  
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all,setAll] = useState(0)
-
-  const avg = (good+-bad)/all ? (good-bad)/all : 0
-  const pos = `${good/all ? good/all*100 : 0} %`
-  
-  const handleButtonClick = (value) => {
-      switch (value){
-        case 1:
-          setGood(good+1)
-          setAll(all+1)
-          break;
-
-        case 0:
-          setNeutral(neutral+1)
-          setAll(all+1)
-          break;
-
-        case -1:
-          setBad(bad+1)
-          setAll(all+1)
-          break;
-
-        default:
-            break;
-      }
-    }
-
-
+const Anecdote = ({text,votes}) => {
   return (
     <div>
-
-      <Title text="Give Feedback"/>
-      <Button type = {"good"} handleClick = {()=>handleButtonClick(1)}/>
-      <Button type = {"neutral"} handleClick = {()=>handleButtonClick(0)}/>
-      <Button type = {"bad"} handleClick = {()=>handleButtonClick(-1)}/>
-      <Title text="Statistics"/>
-      <table>
-        <tbody>
-          <Statistics text="Good" value={good}/>
-          <Statistics text="Neutral" value={neutral}/>
-          <Statistics text="Bad" value={bad}/>
-          <Statistics text="all" value={all}/>
-          <Statistics text="average" value={avg}/>
-          <Statistics text="positive" value={pos}/>
-        </tbody>
-      </table>
-
+      <p>{text}<br/>Has {votes} votes</p>
     </div>
-    
-
   )
 }
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes,setVotes] = useState(Array(props.anecdotes.length).fill(0))
+
+  const addVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+  const generateAnecdote = () => {
+    let aux = selected
+
+    while(aux===selected){
+      aux = randomInt(props.anecdotes.length-1) 
+    }
+    // Primero se ejecuta la función y luego se vuelve a renderizar
+    setSelected(aux)
+    console.log(selected)
+    }
+  
+  const randomInt = (max,min=0) => {
+    return Math.floor(Math.random()*(max-min+1)+min)
+  }
+
+  const maxPosition = votes.findIndex(element => element === Math.max(...votes))
+ 
+  return (
+    <div>
+      <Title text="Anecdote of the day"/>
+      <Anecdote text={props.anecdotes[selected]} votes = {votes[selected]}/>
+      <Button handleClick={addVote} text="Vote"/>
+      <Button handleClick={generateAnecdote} text="Next anecdote"/>
+      <Title text="Anecdote with most votes"/>
+      <Anecdote text={props.anecdotes[maxPosition]} votes = {votes[maxPosition]}/>
+    </div>
+  )
+}
+
+
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App anecdotes={anecdotes}/>
   </React.StrictMode>
 );
+  // Secuencia para configurar user snippet File > Preferences > user snippets > javascript
+ // Snippet para console.log()
+  // {
+  //   "console.log": {
+  //     "prefix": "clog",
+  //     "body": [
+  //       "console.log('$1')",
+  //     ],
+  //     "description": "Log output to console"
+  //   }
+  // }
