ed835c09a11577a6f28ac1b296db7d2bb97a413f
alejandro <alejandro9.ch321@gmail.com>
2022-08-27 23:52:36 -0500

    Sexta parte del proyecto: Ejercicio 2.11

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from 'axios';
+
 
 const App = () => {
-  const [persons, setPersons] =useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] =useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter,setFilter] = useState('')
   const [showAll,setShowAll] = useState(true)
 
   const personsToShow = showAll ? persons: persons.filter(person => RegExp(`^${filter}`,"gi").test(person.name) )
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+,[])
+
+  // Usando fetch en lugar de axios
+  useEffect(() => {
+    fetch('http://localhost:3001/persons')
+    .then(response => response.json())
+      .then(data => {
+        setPersons(data)
+      })
+  }
+  ,[])
 
   const addPerson = (event)=>{
     event.preventDefault()
